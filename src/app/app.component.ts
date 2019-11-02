e2089e3370b3d12ada9e947c4494805eb2d55aff
kossy0701 <0421kossy0421@gmail.com>
2019-11-02 22:01:14 +0900

    add mock and push-chat-function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { Comment } from './class/comment';
+import { User } from './class/user';
+
+const CURRENT_USER: User = new User(1, '田中太郎');
+const ANOTHER_USER: User = new User(2, '山田二郎');
+
+const COMMENTS: Comment[] = [
+  new Comment(ANOTHER_USER, 'お疲れ様です！'),
+  new Comment(ANOTHER_USER, '寒いっすね'),
+  new Comment(CURRENT_USER, 'キンタマが縮むな'),
+  new Comment(ANOTHER_USER, 'タマヒュンですね'),
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chat-works';
+  comments =  COMMENTS;
+  currentUser = CURRENT_USER;
+  content = '';
+
+  addComment(comment: string): void{
+    if (comment) {
+      this.comments.push(new Comment(this.currentUser, comment));
+    }
+  }
 }
