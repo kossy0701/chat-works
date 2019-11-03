7419f36f9001c0dd374ec1c86f18f05e4e562a01
kossy0701 <0421kossy0421@gmail.com>
2019-11-03 21:31:03 +0900

    add some module and split component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
+import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { ChatComponent } from './chat/chat.component';
 import { AppComponent } from './app.component';
-import { CommentDatePipe } from './pipe/comment-date.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentDatePipe
+    ChatComponent
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    SharedModule,
     AppRoutingModule,
-    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+
+export class CoreModule {
+
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only'
+      );
+    }
+  }
+}
