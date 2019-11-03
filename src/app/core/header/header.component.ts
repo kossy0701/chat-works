81f6fa7a17e9e7d245578790ea64682d47957af9
kossy0701 <0421kossy0421@gmail.com>
2019-11-04 00:56:07 +0900

    add logut methods and manage session

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../service/session.service';
+
+import { Session } from '../../class/user';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public login = false;
+
+  constructor(public sessionService: SessionService) { }
 
   ngOnInit() {
+    this.sessionService.sessionState.subscribe((session: Session) => {
+      if (session) {
+        this.login = session.login;
+      }
+    });
+  }
+
+  logout(): void {
+    this.sessionService.logout();
   }
 
 }
