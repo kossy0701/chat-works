b8c0f5b5c32e8df685cca0dcf986510584a4bf0f
kossy0701 <0421kossy0421@gmail.com>
2019-11-10 22:20:54 +0900

    add loginFunction

--- src/app/class/user.ts
+++ src/app/class/user.ts
@@ -25,3 +25,21 @@ export class Session {
   }
 
 }
+
+export class Password {
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+
+  constructor() {
+    this.email = '';
+    this.password = '';
+    this.passwordConfirmation = '';
+  }
+
+  reset(): void {
+    this.email = '';
+    this.password = '';
+    this.passwordConfirmation = '';
+  }
+}
--- src/app/core/service/session.service.ts
+++ src/app/core/service/session.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 
-import { Session } from '../../class/user';
+import { Session, Password } from '../../class/user';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -14,16 +15,48 @@ export class SessionService {
   public sessionSubject = new Subject<Session>();
   public sessionState = this.sessionSubject.asObservable();
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private angularFireAuth: AngularFireAuth) {}
 
-  login(): void {
-    this.session.login = true;
-    this.sessionSubject.next(this.session);
-    this.router.navigate(['/']);
+  login(account: Password): void {
+    this.angularFireAuth.auth.signInWithEmailAndPassword(account.email, account.password)
+      .then(auth => {
+        // メールアドレスが有効かどうか
+        if (!auth.user.emailVerified) {
+          this.angularFireAuth.auth.signOut();
+          return Promise.reject('メールアドレスが確認できていません。');
+        } else {
+          this.session.login = true;
+          this.sessionSubject.next(this.session);
+          return this.router.navigate(['/']);
+        }
+      })
+      .then(() => alert('ログインしました。'))
+        .catch(err => {
+          console.log(err);
+          alert('ログインに失敗しました。\n' + err);
+        });
   }
 
   logout(): void {
-    this.sessionSubject.next(this.session.reset());
-    this.router.navigate(['/account/login']);
+    this.angularFireAuth.auth.signOut()
+      .then(() => {
+        this.sessionSubject.next(this.session.reset());
+        return this.router.navigate(['/account/login']);
+      })
+        .then(() => alert('ログインしました'))
+          .catch(err => {
+            console.log(err);
+            alert('ログアウトに失敗しました。\n' + err);
+          });
+  }
+
+  signUp(account: Password): void {
+    this.angularFireAuth.auth.createUserWithEmailAndPassword(account.email, account.password) // アカウント作成
+      .then(auth => auth.user.sendEmailVerification()) // アカウントを有効化
+        .then(() => alert('メールアドレス確認メールを送信しました。'))
+          .catch(err => {
+            console.log(err);
+            alert('アカウントの作成に失敗しました。\n' + err);
+          });
   }
 }
