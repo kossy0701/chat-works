68dc3a35b42016e3dd59916065ba650288e8ae50
kossy0701 <0421kossy0421@gmail.com>
2019-11-10 22:19:51 +0900

    add angular-materials

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { AccountRoutingModule } from './account-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [LoginComponent, SignUpComponent],
   imports: [
-    CommonModule,
-    AccountRoutingModule
+    SharedModule,
+    AccountRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule
   ]
 })
 export class AccountModule { }
